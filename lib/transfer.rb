982afbe3974e015270dfd43d89b783d449e3307b
Caio Sanches <ccampato@gmail.com>
2018-06-05 02:16:21 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,8 @@
 class Transfer
-  attr_accessor :name
+  attr_accessor :sender
 
-  def initialize(name, transfer, amount)
-    @name = name
+  def initialize(sender, transfer, amount)
+    @sender = sender
   end
 
 end
